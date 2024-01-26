be41431fe15cc953fca270507c5dd72e664bebe1
GusFoobar <gugatpm@gmail.com>
2024-01-26 02:31:46 +0000

    Trying to fix this horrible plugin

--- plugin/transparent.lua
+++ plugin/transparent.lua
@@ -1,1 +1,47 @@
-require('colors')
+-- Groups supposed to be transparent
+---@type table<string>
+local groups = {
+    "Normal",
+    "NormalNC",
+    "SignColumn",
+    "NormalFloat",
+    "FloatBorder",
+    "Pmenu",
+    "TelescopeNormal",
+    "TelescopePromptNormal",
+    "TelescopeBorder",
+    "LineNr",
+    "EndOfBuffer",
+    "FloatTitle",
+    "ZenBg",
+    "WinBarNC",
+    "WinBar",
+    "WinSeparator",
+    "TabLineFill",
+    "TabLine",
+    "CursorLine",
+    "CursorLineNr",
+    "TelescopePreviewTitle",
+    "TelescopePromptTitle",
+    "TelescopeResultsTitle",
+    "TelescopePromptBorder",
+    "TelescopePromptPrefix",
+}
+
+-- Method that actually makes the stuff above transparent.
+function ColorMyPencils()
+    for _, i in pairs(groups) do
+        vim.api.nvim_set_hl(0, i, { bg = 'none' })
+    end
+
+    -- Getting colorscheme for checking some stuff,
+    -- we are basically making sure rose-pine does not use certain colors
+    local colorscheme = vim.g.colors_name
+    if colorscheme == 'rose-pine' then
+        vim.api.nvim_set_hl(0, "ColorColumn", { bg = "#2F2F2F" })
+    end
+
+    vim.api.nvim_set_hl(0, "CursorLineNr", { fg = 'none' })
+end
+
+ColorMyPencils()
