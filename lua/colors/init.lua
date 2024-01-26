5eae7d27b6faaf08306c7831343a566c33d8f10a
GusFoobar <gugatpm@gmail.com>
2024-01-26 01:30:26 +0000

    First implementation

---
+++ lua/colors/init.lua
@@ -0,0 +1,53 @@
+M = {}
+
+-- Groups supposed to be transparent
+---@type table<string>
+local groups = {
+    "Normal",
+    "NormalNC",
+    "SignColumn",
+    "NormalFloat",
+    "FloatBorder",
+    "Pmenu",
+    "TelescopeNormal",
+    "TelescopePromptNormal",
+    "TelescopeBorder",
+    "LineNr",
+    "EndOfBuffer",
+    "FloatTitle",
+    "ZenBg",
+    "WinBarNC",
+    "WinBar",
+    "WinSeparator",
+    "TabLineFill",
+    "TabLine",
+    "CursorLine",
+    "CursorLineNr",
+    "TelescopePreviewTitle",
+    "TelescopePromptTitle",
+    "TelescopeResultsTitle",
+    "TelescopePromptBorder",
+    "TelescopePromptPrefix",
+}
+
+-- Method that actually makes the stuff above transparent.
+local function colorMyPencils()
+    for _, i in pairs(groups) do
+        vim.api.nvim_set_hl(0, i, { bg = 'none' })
+    end
+
+    -- Getting colorscheme for checking some stuff,
+    -- we are basically making sure rose-pine does not use certain colors
+    local colorscheme = vim.g.colors_name
+    if colorscheme == 'rose-pine' then
+        vim.api.nvim_set_hl(0, "ColorColumn", { bg = "#2F2F2F" })
+    end
+
+    vim.api.nvim_set_hl(0, "CursorLineNr", { fg = 'none' })
+end
+
+function M.setup()
+    colorMyPencils()
+end
+
+return M
